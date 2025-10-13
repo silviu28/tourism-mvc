5455b93764ecf590b1a7011f3b7f3fcebfe62c71
Silviu <183800984+silviu28@users.noreply.github.com>
2025-10-13 08:48:03 +0300

    add index view

---
+++ views/index/script.js
@@ -0,0 +1,34 @@
+const image = document.getElementById("image");
+
+const images = [
+  "/images/gallery/gallery1.jpg",
+  "/images/gallery/gallery2.jpg",
+  "/images/gallery/gallery3.jpg",
+  "/images/gallery/gallery4.jpeg",
+  "/images/gallery/gallery5.jpg"
+];
+
+const sendButton = document.getElementById("sendbutton");
+const topbutton = document.getElementById("topButton");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topbutton.style.display = "block";
+  } else {
+    topbutton.style.display = "none";
+  }
+};
+
+const toTop = function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+setInterval(function () {
+  let random = Math.floor(Math.random() * 5);
+  image.src = images[random];
+}, 5000);
+
+sendbutton.onclick = function () {
+  window.alert("Form has been sent!");
+}
