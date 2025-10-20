0877254e26ec4cbf53f09b24428c063d57a356c7
Silviu <183800984+silviu28@users.noreply.github.com>
2025-10-20 09:17:53 +0300

    config static serve and db

---
+++ controllers/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const { Sequelize, QueryTypes } = require('sequelize');
+const { MySqlDialect } = require('@sequelize/mysql');
+require('dotenv').config({ quiet: true });
+// const cors = require('cors');
+
+const PORT = 4004;
+
+const app = express();
+app.use(express.static(
+  path.join(__dirname, '..', 'views')
+));
+app.use(express.json());
+
+const sequelize = new Sequelize({
+  dialect: 'mysql',
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.PASSWORD || '',
+  host: 'localhost',
+  port: 3306,
+});
+
+app.get('/users', async (req, res) => {
+  const users = await sequelize.query('SELECT * FROM users', {
+    type: QueryTypes.SELECT,
+  });
+  res.json(users);
+});
+
+app.listen(PORT, () => {
+  console.log(`--> localhost:${PORT}`);
+});
