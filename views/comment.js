8df6da1cea16261743ccd2dd0f3886cfdc9a62c6
Silviu <183800984+silviu28@users.noreply.github.com>
2025-10-20 13:10:04 +0300

    refactor project structure

--- views/comment.js
+++
@@ -1,6 +0,0 @@
-console.log('hello from comment section');
-
-const comments = document.getElementById('comment-section');
-const test = document.createElement('p');
-test.innerText = 'yep';
-comments.appendChild(test);
--- views/signup.js
+++
@@ -1,9 +0,0 @@
-const submitForm = function (event) {
-  event.preventDefault();
-  const name = document.getElementById('namefield').textContent;
-  const email = document.getElementById('emailfield').textContent;
-  const password = document.getElementById('passwordfield').textContent;
-
-  // server POST here...
-  alert('signup successful');
-}
