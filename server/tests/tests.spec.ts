69e903ff3e89ac7fb0db807880f2604cef974d46
Silviu <183800984+silviu28@users.noreply.github.com>
2025-11-01 12:29:02 +0200

    add more unit tests

--- server/tests/tests.spec.ts
+++ server/tests/tests.spec.ts
@@ -1,3 +1,4 @@
+import { Admin } from "../models/Admin";
 import { Comment } from "../models/Comment";
 import { Image } from "../models/Image";
 import { User } from "../models/User";
@@ -5,7 +6,6 @@ import sequelize from "./testSequelizeConfig";
 
 beforeAll(async () => {
   await sequelize.authenticate();
-  await sequelize.sync({ force: true });
 });
 
 afterAll(async () => {
@@ -14,14 +14,15 @@ afterAll(async () => {
 
 describe("Server IO", () => {
   beforeEach(async () => {
-    await sequelize.truncate({ cascade: true });
+    await sequelize.sync({ force: true });
   });
 
   it("should sign up a user", async () => {
     const user = await User.create({
       username: "mich",
       passwordHash: "passwordHashLol",
-      email: "mich@email.com"
+      email: "mich@email.com",
+      notify: false
     });
 
     expect(user.username).toBe("mich");
@@ -29,18 +30,21 @@ describe("Server IO", () => {
     expect(user.email).toBe("mich@email.com");
     expect(user.admin).toBeUndefined();
     expect(user.fullName).toBeUndefined();
+    expect(user.notify).toBe(false);
   });
 
   it("should add and load comments", async () => {
     await Comment.create({
       userId: 1,
-      comment: "this is a comment"
+      comment: "this is a comment",
+      date: new Date(),
     });
 
     const comments = await Comment.findAll();
 
     expect(comments[0].id).toBeDefined();
     expect(comments[0].comment).toBe("this is a comment");
+    expect(comments[0].date).toBeDefined();
     expect(comments[0].userId).toBeDefined();
   });
 
@@ -53,5 +57,59 @@ describe("Server IO", () => {
 
     expect(images[0].id).toBeDefined();
     expect(images[0].src).toBe("somePictureSrc");
+  });
+
+  it("should add an admin and validate their clearance", async () => {
+    await User.create({
+      username: "mich",
+      passwordHash: "passwordHashLol",
+      email: "mich@email.com",
+      notify: false,
+    });
+    const user = await User.findOne({
+      where:
+        { username: "mich" }
+    });
+
+    await Admin.create({
+      userId: user!.id
+    });
+    const admin = await Admin.findOne({
+      where:
+        { userId: user!.id }
+    });
+
+    expect(admin).toBeDefined();
+  });
+
+  it("should cascade user deletion to admin", async () => {
+    await User.create({
+      username: "mich",
+      passwordHash: "passwordHashLol",
+      email: "mich@email.com",
+      notify: false,
+    });
+    const user = await User.findOne({
+      where:
+        { username: "mich" }
+    });
+
+    await Admin.create({
+      userId: user!.id
+    });
+
+    await User.destroy({
+      where: {
+        username: "mich"
+      }
+    });
+
+    const admin = await Admin.findOne({
+      where: {
+        id: user!.id
+      }
+    });
+
+    expect(admin).toBeNull();
   });
 });
