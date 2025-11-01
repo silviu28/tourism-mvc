a778f875377413f283f3b7883d017b4063d7e93a
Silviu <183800984+silviu28@users.noreply.github.com>
2025-11-01 18:21:24 +0200

    fetch data to price table

--- view/src/components/PriceTable/index.tsx
+++ view/src/components/PriceTable/index.tsx
@@ -1,37 +1,57 @@
-import type { FC } from "react";
+import { useContext, type FC } from "react";
 import type { Price } from "../../types";
+import { useQuery } from "@tanstack/react-query";
+import UserContext from "../../UserContext";
+import axios from "axios";
 
 interface PriceTableProps {
   prices: Price[],
 };
 
-const PriceTable: FC<PriceTableProps> = ({ prices }) => {
+const PriceTable: FC<PriceTableProps> = () => {
+  const [, , setAlert] = useContext(UserContext);
+
+  const { data: prices = [], isLoading } = useQuery<Price[]>({
+    queryKey: ["prices"],
+    queryFn: async () => {
+      try {
+        const pricesRes = await axios.get("http://localhost:4004/prices");
+        return pricesRes.data;
+      } catch (error) {
+        setAlert("Unable to load prices", "", true);
+      }
+    }
+  });
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <>
       <h1>Price Table</h1>
       <table width="100%">
         <tbody>
           <tr className={"pricetable"}>
-            <th>Location</th>
             <th>Country</th>
+            <th>Available?</th>
             <th>Status</th>
             <th>Insurance</th>
             <th>Travel Host</th>
             <th>Pricing</th>
-            {prices?.map(price =>
-              <tr>
-                <td>{price.location}</td>
-                <td>{price.country}</td>
-                <td>{price.status}</td>
-                <td>{price.insurance}</td>
-                <td>{price.travelHost}</td>
-                <td>
-                  {price.priceLower && price.priceUpper
-                    ? `$${price.priceLower} - $${price.priceUpper}`
-                    : "Unspecified"}
-                </td>
-              </tr>)}
           </tr>
+          {prices?.map(price =>
+            <tr>
+              <td>{price.country}</td>
+              <td>{price.isAvailable ? "yes" : "no"}</td>
+              <td>{price.status}</td>
+              <td>{price.insurance}</td>
+              <td>{price.travelHost}</td>
+              <td>
+                {(price.priceLower && price.priceUpper) &&
+                  `${price.priceLower} - ${price.priceUpper}`}
+              </td>
+            </tr>)}
         </tbody>
       </table>
       <br />
