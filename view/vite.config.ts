d03c047eb2a3bb27096e3ca81d0b7218b41461d5
Silviu <183800984+silviu28@users.noreply.github.com>
2026-09-11 17:04:31 +0300

    Configure Playwright

--- view/vite.config.ts
+++ view/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, type UserConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
@@ -9,4 +9,4 @@ export default defineConfig({
     globals: true,
     setupFiles: './test_setup.js',
   }
-});
+} as UserConfig);
